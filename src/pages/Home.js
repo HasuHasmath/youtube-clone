20e7eac58c07796d8cfb7ca34ad2d0779f05f302
hasuhasmath <hasuh5pepe@gmail.com>
2024-05-07 14:36:45 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
 import { CategoryItems } from "../static/data";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "../firebase";
+import { Link } from "react-router-dom";
+import Video from "../components/Video";
 
 const Home = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "videos"));
+    onSnapshot(q, (snapShot) => {
+      setVideos(
+        snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    });
+  }, []);
+  // console.log(videos);
   return (
     <>
       <Sidebar />
@@ -16,10 +34,24 @@ const Home = () => {
               {item}
             </h2>
           ))}
-        </div> 
         </div>
+        <div className="pt-12  px-5 grid grid-cols-yt gap-x-3 gap-y-8">
+          {
+            videos.length === 0 ? (
+              <div className="h-[86vh]"></div>
+            ) : (
+              videos.map((video,i) => (
+                  <Link to={`/video/${video.id}`} key={video.id}>
+                    <Video {...video}/>
+                  </Link>
+                  
+              ))
+            )
+          }
+        </div>
+      </div>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
